29f5534b5917865700c4da42aab5e35bbe1ed063
Thomas Egebakken Honnemyr <thomaseho@uia.no>
2020-10-30 13:25:28 +0100

    added some more controllers and views, so that more people can enter the project next sprint

---
+++ MEG_Boosting_Site/Controllers/OverwatchController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MEG_Boosting_Site.Controllers
+{
+    public class OverwatchController : Controller
+    {
+        // GET
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
