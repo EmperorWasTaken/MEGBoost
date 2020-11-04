77f23a0152486e709756ec6b87d9957cdcbe7c83
Thomas Egebakken Honnemyr <thomaseho@uia.no>
2020-11-04 15:26:34 +0100

    Added some basic functionality to the worldofwarcraft controller, and to the views presenting a list of the boosts we have in our database for that service. this is temporary and not how the view will end up

--- MEG_Boosting_Site/Controllers/WorldOfWarcraftController.cs
+++ MEG_Boosting_Site/Controllers/WorldOfWarcraftController.cs
@@ -1,19 +1,36 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Linq;
+using System.Threading.Tasks;
+using MEG_Boosting_Site.Data;
+using MEG_Boosting_Site.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace MEG_Boosting_Site.Controllers
 {
     public class WorldOfWarcraftController : Controller
     {
-        // GET
-        public IActionResult ClassicIndex()
+        private readonly ApplicationDbContext _db;
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public WorldOfWarcraftController(ApplicationDbContext db, UserManager<ApplicationUser> userManager)
         {
-            return View();
+            _db = db;
+            _userManager = userManager;
+        }
+
+        // GET
+        public async Task<ActionResult> ClassicIndex()
+        {
+            var WoWClassic = _db.Products.Where(n => n.Service.Equals("WoWClassic")).ToListAsync();
+            return View(await WoWClassic);
         }
         
         // GET
-        public IActionResult RetailIndex()
+        public async Task<IActionResult> RetailIndex()
         {
-            return View();
+            var WoWRetail = _db.Products.Where(s => s.Service.Equals("WoWRetail")).ToListAsync();
+            return View(await WoWRetail);
         }
     }
 }
