e9e87f5d89d19d03fae8790aa5a617a1598c2176
Kristian Utvær <kristianu@uia.no>
2020-11-06 16:20:01 +0100

    La til tid på custom orders

--- MEG_Boosting_Site/Models/CustomOrder.cs
+++ MEG_Boosting_Site/Models/CustomOrder.cs
@@ -7,16 +7,27 @@ namespace MEG_Boosting_Site.Models
 {
     public class CustomOrder
     {
-        public CustomOrder(){}
+        
+        public CustomOrder()
+        {
+            Time = DateTime.Now.ToString("dd.MM.yyyy HH:mm:ss");
+        }
         
         // Unique identifier for the product 
         public int Id { get; set; }
         
         // A name for the specific service, for example: WoWRetailGladiator
+        [Required, StringLength(50), DisplayName("Name")]
         public string Name { get; set; }
         
         // Details about the service
+        [Required, StringLength(5000), DisplayName("Details")]
         public string Details { get; set; }
+        
+        // Sets date and time
+        [Required, DataType(DataType.DateTime)]
+        public string Time { get; set; }
+        
         
         public ApplicationUser ApplicationUser { get; set; }
     }
