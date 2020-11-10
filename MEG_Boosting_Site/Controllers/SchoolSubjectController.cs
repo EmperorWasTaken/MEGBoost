4d9b1c36e5b288379a6ca09e17dec6b634178613
Thomas Egebakken Honnemyr <thomaseho@uia.no>
2020-11-10 11:58:14 +0100

    changed the queries in the controller so it asks for the right thing, changed the views so that they can display lists

--- MEG_Boosting_Site/Controllers/SchoolSubjectController.cs
+++ MEG_Boosting_Site/Controllers/SchoolSubjectController.cs
@@ -23,43 +23,43 @@ namespace MEG_Boosting_Site.Controllers
         // GET
         public async Task<IActionResult> ROne()
         {
-            var ROne = _db.Products.Where(n => n.Service.Equals("ROne")).ToListAsync();
+            var ROne = _db.Products.Where(n => n.Service.Equals("R1")).ToListAsync();
             return View(await ROne);
         }
         
         public async Task<IActionResult> RTwo()
         {
-            var RTwo = _db.Products.Where(n => n.Service.Equals("RTwo")).ToListAsync();
+            var RTwo = _db.Products.Where(n => n.Service.Equals("R2")).ToListAsync();
             return View(await RTwo);
         }
         
         public async Task<IActionResult> MOne()
         {
-            var MOne = _db.Products.Where(n => n.Service.Equals("MOne")).ToListAsync();
+            var MOne = _db.Products.Where(n => n.Service.Equals("M1")).ToListAsync();
             return View(await MOne);
         }
         
         public async Task<IActionResult> MTwo()
         {
-            var MTwo = _db.Products.Where(n => n.Service.Equals("MTwo")).ToListAsync();
+            var MTwo = _db.Products.Where(n => n.Service.Equals("M2")).ToListAsync();
             return View(await MTwo);
         }
 
         public async Task<IActionResult> MThree()
         {
-            var MThree = _db.Products.Where(n => n.Service.Equals("MThree")).ToListAsync();
+            var MThree = _db.Products.Where(n => n.Service.Equals("M3")).ToListAsync();
             return View(await MThree);
         }
         
         public async Task<IActionResult> DiscreteMOne()
         {
-            var DMOne = _db.Products.Where(n => n.Service.Equals("DMOne")).ToListAsync();
+            var DMOne = _db.Products.Where(n => n.Service.Equals("DM1")).ToListAsync();
             return View(await DMOne);
         }
         
         public async Task<IActionResult> DiscreteMTwo()
         {
-            var DMTwo = _db.Products.Where(n => n.Service.Equals("DMTwo")).ToListAsync();
+            var DMTwo = _db.Products.Where(n => n.Service.Equals("DM2")).ToListAsync();
             return View(await DMTwo);
         }
     }
