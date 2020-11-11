ba3279ab404bbad13664650284c2c36dbb09e50e
Thomas Egebakken Honnemyr <thomaseho@uia.no>
2020-11-11 16:43:18 +0100

    Added a sidebar to the site, moved reviews down from the navbar to the sidebar, added some information views to home. Added controller functions to access the views in homecontroller. Added some icons to the navbar

--- MEG_Boosting_Site/Controllers/HomeController.cs
+++ MEG_Boosting_Site/Controllers/HomeController.cs
@@ -28,6 +28,21 @@ namespace MEG_Boosting_Site.Controllers
             return View();
         }
 
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
+        public IActionResult FAQ()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
