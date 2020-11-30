4800eb18dbcebd98d254030a5d99c9dec0fc2376
Thomas Egebakken Honnemyr <thomaseho@uia.no>
2020-11-30 18:27:26 +0100

    added a property to product and review to see if it should be on the homepage or not, added partial views for bestselling products and top reviews, added some dummy text to the main page to get a feel for how it will look

---
+++ MEG_Boosting_Site/Models/BestsellersTopReviewsViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace MEG_Boosting_Site.Models
+{
+    public class BestsellersTopReviewsViewModel
+    {
+        public List<Product> Products { get; set; }
+        public List<Review> Reviews { get; set; }
+    }
+}
